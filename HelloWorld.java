a557c560d958ccc21e6025fa6d6d2ea0dcb2b8b3
name_lgy <806903281@qq.cpm>
2019-10-24 15:26:10 +0800

    15:26

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,5 +6,7 @@ public class HelloWorld{
 		System.out.println("2");
 		System.out.println("3");
         System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
 	}	
 }
